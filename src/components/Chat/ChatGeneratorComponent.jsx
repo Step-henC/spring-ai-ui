3e8202f35875d5e512f6719f9a136d2daf3140e2
Step-henC <stevecunningham@outlook.com>
2024-12-29 20:52:01 -0600

    chat and recipe page

--- src/components/Chat/ChatGeneratorComponent.jsx
+++ src/components/Chat/ChatGeneratorComponent.jsx
@@ -1,7 +1,32 @@
 
-
+import { useState } from "react";
 export default function ChatGeneratorComponent() {
+  const [prompt, setPrompt] = useState("");
+  const [response, setResponse] = useState('');
+
+  const askAI = async () => {
+    try { 
+      const res = await fetch(`http://localhost:8080/v1/ask-ai?prompt=${prompt}`);
+      const data = await res.text();
+      setResponse(data);
+
+    } catch (e) {
+      console.log('Error ', e );
+    }
+  }
   return (
-    <h2>Let us Chat</h2>
+    <div>
+    <h2>Talk to AI</h2>
+    <input 
+    type="text" 
+    value={prompt}
+    onChange={(e) => setPrompt(e.target.value)}
+    placeholder="Enter a prompt for AI"
+    />
+    <button onClick={askAI}>Ask AI</button>
+    <div className="output">
+      <p>{response}</p>
+    </div>
+    </div>
   )
 }
--- src/components/RecipeGenerator/RecipeGeneratorComponent.jsx
+++ src/components/RecipeGenerator/RecipeGeneratorComponent.jsx
@@ -1,7 +1,46 @@
+import { useState } from "react";
 
 
 export default function RecipeGeneratorComponent() {
+  const [ingredients, setIngredients] = useState('');
+  const [cuisine, setCuisine] = useState('');
+  const [dietRestrictions, setDietRestrictions] = useState('');
+  const [recipe, setRecipe] = useState('');
+
+  const createRecipe = async () => {
+    try {
+      const res =  await fetch(`http://localhost:8080/v1/recipe-creator?ingredients=${ingredients}&cuisine=${cuisine}&dietaryRestrictions=${dietRestrictions}`);
+      const data = await res.text();
+      setRecipe(data);
+    } catch (e) {
+      console.log('Error ', e);
+    }
+  }
   return (
-    <h2>Let us make a healthy recipe!</h2>
+    <div>
+    <h2>Create a Recipe</h2>
+    <input 
+    type="text"
+    value={ingredients}
+    onChange={(e) => setIngredients(e.target.value)}
+    placeholder="Enter ingredients"
+    />
+       <input 
+    type="text"
+    value={dietRestrictions}
+    onChange={(e) => setDietRestrictions(e.target.value)}
+    placeholder="Enter dietary restrictions"
+    />
+       <input 
+    type="text"
+    value={cuisine}
+    onChange={(e) => setCuisine(e.target.value)}
+    placeholder="Enter cuisine type"
+    />
+    <button onClick={createRecipe}>Create Recipe</button>
+    <div className="output">
+      <pre className="recipe-text">{recipe}</pre>
+    </div>
+    </div>
   )
 }
